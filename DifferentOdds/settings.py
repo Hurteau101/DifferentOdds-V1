9ee288c21f638e11eb3066bd5194ab3459b8c18a
Devon <devonhurteau@gmail.com>
2024-10-10 15:58:32 -0600

    Changed debug

--- DifferentOdds/settings.py
+++ DifferentOdds/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["https://differentodds-3cceca9b5971.herokuapp.com/", "differentodds-3cceca9b5971.herokuapp.com", "127.0.0.1"]
 
