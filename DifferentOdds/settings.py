70812b93b463d211f6f22a2531e359a525b2bb3f
Devon <DevonHurteau@gmail.com>
2024-10-12 00:16:06 -0500

    Created starting admin_panel app

--- DifferentOdds/settings.py
+++ DifferentOdds/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'esports.apps.EsportsConfig',
     'users.apps.UsersConfig',
+    'admin_panel.apps.AdminPanelConfig'
 ]
 
 MIDDLEWARE = [
