f42df520005716e0b990673bb85bdff2fa8bc151
Devon <DevonHurteau@gmail.com>
2024-10-12 02:40:27 -0500

    Added basic admin panel

--- DifferentOdds/urls.py
+++ DifferentOdds/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
 
+from admin_panel import views
+
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('esports/', include('esports.urls')),
     path('', include('users.urls')),
-    path('', lambda request: redirect('login'))
+    path('', lambda request: redirect('login')),
+    path('admin-panel', views.admin_panel, name='admin_panel'),
 ]
