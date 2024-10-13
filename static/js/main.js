578169ab381a992ec8ff76ce75cc8828428b4761
Devon <DevonHurteau@gmail.com>
2024-10-13 09:05:17 -0500

    Added ordering in datatables

--- static/js/main.js
+++ static/js/main.js
@@ -84,9 +84,10 @@ $(document).ready(function() {
         responsive: true,
         searching: true,  // Use 'searching' instead of 'bFilter'
         lengthMenu: [[-1, 10, 25, 50], ["All", 10, 25, 50]],
-        order: [[0, "desc"]],
+        // order: [[0, "desc"]],
         autoWidth: false,  // Use 'autoWidth' instead of 'bAutoWidth'
         info: true,
+        ordering: true,
         buttons: [
             {
                 text: 'Refresh',
